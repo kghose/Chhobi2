386b93465d35e08e0f77fd49bc3a1cd494f78696
Kaushik Ghose <kaushik.ghose@gmail.com>
2013-06-23 14:00:59 -0400

    Implementing events, including virtual listing

--- dirbrowser.py
+++ dirbrowser.py
@@ -5,21 +5,26 @@ By mmgp (http://stackoverflow.com/users/1832154/mmgp)
 import os, sys, Tkinter as tki, ttk
 
 class DirBrowse(tki.Frame):
-  def __init__(self, parent, root='./', **options):
+  def __init__(self, parent, dir_root='./', **options):
     tki.Frame.__init__(self, parent, **options)
-
     self.treeview = ttk.Treeview(columns=("fullpath", "type"),show='tree',displaycolumns=())
     self.treeview.pack(fill='both', expand=True)
-    self.set_root(root)
+    self.set_dir_root(dir_root)
     self.treeview.bind('<<TreeviewOpen>>', self.update_tree)
 
-  def set_root(self, startpath):
+  def set_dir_root(self, startpath):
+    map(self.treeview.delete, self.treeview.get_children()) #Delete the original
+    self.start_path = startpath
     dfpath = os.path.abspath(startpath)
     node = self.treeview.insert('', 'end', text=dfpath,
                            values=[dfpath, "directory"], open=True)
     self.fill_tree(node)
 
   def fill_tree(self, node):
+    if self.treeview.set(node, "type") == 'back to root': #Special top node for virtual listing
+      self.set_dir_root(self.start_path)
+      return
+
     if self.treeview.set(node, "type") != 'directory':
       return
 
@@ -35,9 +40,21 @@ class DirBrowse(tki.Frame):
         ptype = 'directory'
 
       fname = os.path.split(p)[1]
-      oid = self.treeview.insert(node, 'end', text=fname, values=[p, ptype])
+      oid = self.treeview.insert(node, 'end', text=fname, values=[p, ptype], iid=p)
       if ptype == 'directory':
         self.treeview.insert(oid, 0, text='dummy')
+
+  def virtual_flat(self, files):
+    # Set the contents to a flat listing of files. Useful for 'virtual' folders we create on the fly
+    map(self.treeview.delete, self.treeview.get_children()) #Delete the original
+    ins = self.treeview.insert
+    #Special first node, instructs us to go back to the real listing
+    ptype = 'back to root'
+    ins('','end', text='Back to real listing', values=['real listing', ptype], iid='real listing')
+    ptype = None
+    for file in files:
+      fname = os.path.split(file)[1]
+      ins('','end', text=fname, values=[file, ptype], iid=file)
 
   def update_tree(self, event):
     self.fill_tree(self.treeview.focus())
--- guichhobi.py
+++ guichhobi.py
@@ -1,11 +1,13 @@
 """The GUI consists of six panels.
 -----------------------
 |         A           |
------------------------
+|---------------------|
 |         B           |
------------------------
-|         C     | X| O|
------------------------
+|---------------------|
+|                     |
+|         C           |
+|                     |
+|---------------------|
 |                     |
 |         D           |
 |                     |
@@ -17,14 +19,10 @@
 
 A is the search panel where you form your query. Hitting enter executes the query
 B is the RawQuery window where you can see the RawQuery that will be fed to Spotlight
-C is the path panel that shows the full path the currently selected
-D is the comments pane where you can read and edit the photo comments
-E is the keywords pane where the keywords are listed (one to a line) and can be edited
+C is the directory/file list browser
+D is the comments pane where you can read and edit the photo comments. Hit <CTRL> + S to save, ESC or navigate away to cancel
+E is the keywords pane where the keywords are listed (one to a line) and can be edited. Hit <CTRL> + S to save, ESC or navigate away to cancel
 F is the EXIF pane where a bunch of read only EXIF data is shown
-The X button cancels any changes that have been made
-The O button saves changes
-
-If you edit the caption or the keywords (you will know because the field will change color) selecting a different image will have not update the image metadata display untill you cancel or save the changes
 
 Starting the program with the -d option will print debugger messages to the console
 """
@@ -32,57 +30,37 @@ import logging
 logger = logging.getLogger(__name__)
 import Tkinter as tki, threading, Queue, argparse, time
 import libchhobi as lch
-
-def query_finder_thread(q, stop_event):
-  """q is a Queue object, stop_event is an Event.
-  stop_event from http://stackoverflow.com/questions/6524459/stopping-a-thread-python
-  """
-  selected_files = set([])
-  while not stop_event.is_set():
-    if q.empty():
-      new_selected_files = set(lch.get_paths_of_selected())
-      if new_selected_files != selected_files:
-        selected_files = new_selected_files
-        q.put(selected_files)
-    time.sleep(.2)
+import dirbrowser as dirb
 
 class App(object):
 
   def __init__(self):
     self.root = tki.Tk()
     self.root.wm_title('Chhobi2')
-
     self.setup_window()
-    self.setup_finder_thread()
-    self.start_poll()
-
     self.root.wm_protocol("WM_DELETE_WINDOW", self.cleanup_on_exit)
 
   def cleanup_on_exit(self):
     """Needed to shutdown the polling thread."""
     print 'Window closed. Cleaning up and quitting'
-    self.poll_thread_stop_event.set()
+    #self.poll_thread_stop_event.set()
     self.root.quit() #Allow the rest of the quit process to continue
 
   def setup_window(self):
     self.query_win = tki.Text(self.root, undo=True, width=50, height=3)
     self.query_win['font'] = ('consolas', '12')
     self.query_win.pack(side='top', expand=True, fill='both')
-    self.query_win.insert(tki.INSERT,'Hi there')
+    self.query_win.bind('<Return>', self.search_execute)
+    #self.query_win.insert(tki.INSERT,'Hi there')
 
     self.rawquery_win = tki.Text(self.root, undo=True, width=50, height=2, bg='black', fg='white')
     self.rawquery_win['font'] = ('consolas', '9')
     self.rawquery_win.pack(side='top', expand=True, fill='both')
     self.rawquery_win.insert(tki.INSERT,'Hi there')
 
-    fr = tki.Frame(self.root)
-    fr.pack(side='top', expand=True, fill='both')
-    self.path_win = tki.Text(fr, undo=True, width=30, height=2, bg='gray')
-    self.path_win.pack(side='left', expand=True, fill='both')
-    self.button_cancel = tki.Button(fr, text="X", fg="red")
-    self.button_cancel.pack(side='left')
-    self.button_ok = tki.Button(fr, text="O", fg="red")
-    self.button_ok.pack(side='left')
+    self.dir_win = dirb.DirBrowse(self.root, dir_root='/Users/kghose/Pictures')
+    self.dir_win.pack(side='top', expand=True, fill='both')
+    self.dir_win.treeview.bind("<<TreeviewSelect>>", self.selection_changed)
 
     self.comment_win = tki.Text(self.root, undo=True, width=40, height=3)
     self.comment_win['font'] = ('consolas', '12')
@@ -90,32 +68,22 @@ class App(object):
 
     fr = tki.Frame(self.root)
     fr.pack(side='top', expand=True, fill='both')
-    self.keywords_win = tki.Text(fr, undo=True, width=30, height=10, bg='gray')
+    self.keywords_win = tki.Text(fr, undo=True, width=30, height=10)
     self.keywords_win['font'] = ('consolas', '10')
     self.keywords_win.pack(side='left', expand=True, fill='both')
-    self.exif_win = tki.Text(fr, undo=True, width=30, height=2, bg='gray')
+    self.exif_win = tki.Text(fr, undo=True, width=30, height=2, bg='black', fg='white')
     self.exif_win['font'] = ('consolas', '10')
     self.exif_win.pack(side='left', expand=True, fill='both')
 
-  def setup_finder_thread(self):
-    self.queue = Queue.Queue(maxsize=1)
-    self.poll_interval = 250
-    self.poll_thread_stop_event = threading.Event()
-    self.poll_thread = threading.Thread(target=query_finder_thread, name='FinderPoller', args=(self.queue,self.poll_thread_stop_event))
-    self.poll_thread.start()
+  def selection_changed(self, event):
+    print self.dir_win.treeview.selection()
 
-  def start_poll(self):
-    self._poll_job_id = self.root.after(self.poll_interval, self.poll)
-
-  def stop_poll(self):
-    self.root.after_cancel(self._poll_job_id)
-
-  def poll(self):
-    if self.queue.qsize():
-      self.selected_files = self.queue.get()
-      self.path_win.delete(1.0,tki.END)
-      self.path_win.insert(tki.END, self.selected_files)
-    self._poll_job_id = self.root.after(self.poll_interval, self.poll)
+  def search_execute(self, event):
+    self.dir_win.virtual_flat([
+      '/Users/kghose/Pictures/2011/2011-10-08/DSC_4934.JPG',
+      '/Users/kghose/Pictures/2009/2009-09-25/DSC_1808.JPG',
+      '/Users/kghose/Pictures/2007/2007-06-14/IMG_5018a.JPG',
+    ])
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
