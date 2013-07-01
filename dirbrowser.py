c22a6f0e5139c42f4709f4ce3bf3c1e5ac58865c
Kaushik Ghose <kaushik.ghose@gmail.com>
2013-07-01 00:25:38 -0400

    When we populate the browser we set focus and selection on the first item

--- dirbrowser.py
+++ dirbrowser.py
@@ -12,6 +12,13 @@ class DirBrowse(tki.Frame):
     self.set_dir_root(dir_root)
     self.treeview.bind('<<TreeviewOpen>>', self.update_tree)
 
+  def set_initial_focus(self):
+    tv = self.treeview
+    node = tv.get_children()[0]
+    print node
+    tv.focus(node)
+    tv.selection_set(node)
+
   def set_dir_root(self, startpath):
     map(self.treeview.delete, self.treeview.get_children()) #Delete the original
     self.start_path = startpath
@@ -19,8 +26,7 @@ class DirBrowse(tki.Frame):
     node = self.treeview.insert('', 'end', text=dfpath, iid=dfpath,
                            values=[dfpath, "directory"], open=True)
     self.fill_tree(node)
-    tv = self.treeview
-    tv.selection_set(tv.get_children()[0])
+    self.set_initial_focus()
 
   def fill_tree(self, node):
     if self.treeview.set(node, "type") == 'back to root': #Special top node for virtual listing
@@ -56,11 +62,12 @@ class DirBrowse(tki.Frame):
     ins = self.treeview.insert
     #Special first node, instructs us to go back to the real listing
     ptype = 'back to root'
-    ins('','end', text='Back to real listing', values=['real listing', ptype], iid='real listing')
+    ins('','end', text='Back to real listing', values=['real listing', ptype], iid='backtoroot')
     ptype = None
     for file in files:
       fname = os.path.split(file)[1]
       ins('','end', text=file, values=[file, ptype], iid=file)
+    self.set_initial_focus()
 
   def update_tree(self, event):
     self.fill_tree(self.treeview.focus())
@@ -68,45 +75,3 @@ class DirBrowse(tki.Frame):
   def file_selection(self):
     files = self.treeview.selection()
     return [fi for fi in files if os.path.isfile(fi)]
-
-  def key_command(self, cmd):
-    print cmd
-    response = 'ok'
-    tv = self.treeview
-    sel = tv.selection()
-    if sel == '': #Nothing selected, first thing to do is select something and return
-      tv.selection_set(tv.get_children()[0])
-      return response
-
-    if cmd =='n':
-      next = tv.next(tv.selection())
-      if next == '':#End of leaves OR nothing slected
-        next = tv.parent(tv.selection())
-        if next == '': #End of tree even at root level
-          next = tv.get_children()[0] #Loop to the topmost one
-        else:
-          next = tv.next(next)
-      tv.selection_set(next)
-    elif cmd =='p':
-      prev = tv.prev(tv.selection())
-      if prev == '':#End of leaves
-        prev = tv.parent(tv.selection())
-        if prev == '': #End of tree even at root level
-          prev = tv.get_children()[-1] #Loop to the last one
-      tv.selection_set(prev)
-    elif cmd == '.': #Try and get to leaves
-      tv.focus(tv.selection())
-      self.update_tree(None)
-      next = tv.get_children(tv.selection())
-      if next == '': #Leaf node
-        response = 'leaf'
-      else:
-        tv.selection_set(next[0])
-    elif cmd == ',': #Try and get to parent
-      next = tv.parent(tv.selection())
-      if next == '': #Parent node
-        tv.selection_set(tv.get_children()[0]) #Loop to the topmost one
-      else:
-        tv.selection_set(next)
-    tv.see(tv.selection()) #Make sure we can see this
-    return response #Right now will usually return 'ok' will return 'leaf' for '.' and leaf node
