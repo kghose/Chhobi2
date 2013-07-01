a52a887f1f76351090b0df113f287b75a4c3402e
Kaushik Ghose <kaushik.ghose@gmail.com>
2013-06-30 23:26:46 -0400

    Do we really need the key traps here?

--- dirbrowser.py
+++ dirbrowser.py
@@ -16,9 +16,11 @@ class DirBrowse(tki.Frame):
     map(self.treeview.delete, self.treeview.get_children()) #Delete the original
     self.start_path = startpath
     dfpath = os.path.abspath(startpath)
-    node = self.treeview.insert('', 'end', text=dfpath,
+    node = self.treeview.insert('', 'end', text=dfpath, iid=dfpath,
                            values=[dfpath, "directory"], open=True)
     self.fill_tree(node)
+    tv = self.treeview
+    tv.selection_set(tv.get_children()[0])
 
   def fill_tree(self, node):
     if self.treeview.set(node, "type") == 'back to root': #Special top node for virtual listing
@@ -67,3 +69,44 @@ class DirBrowse(tki.Frame):
     files = self.treeview.selection()
     return [fi for fi in files if os.path.isfile(fi)]
 
+  def key_command(self, cmd):
+    print cmd
+    response = 'ok'
+    tv = self.treeview
+    sel = tv.selection()
+    if sel == '': #Nothing selected, first thing to do is select something and return
+      tv.selection_set(tv.get_children()[0])
+      return response
+
+    if cmd =='n':
+      next = tv.next(tv.selection())
+      if next == '':#End of leaves OR nothing slected
+        next = tv.parent(tv.selection())
+        if next == '': #End of tree even at root level
+          next = tv.get_children()[0] #Loop to the topmost one
+        else:
+          next = tv.next(next)
+      tv.selection_set(next)
+    elif cmd =='p':
+      prev = tv.prev(tv.selection())
+      if prev == '':#End of leaves
+        prev = tv.parent(tv.selection())
+        if prev == '': #End of tree even at root level
+          prev = tv.get_children()[-1] #Loop to the last one
+      tv.selection_set(prev)
+    elif cmd == '.': #Try and get to leaves
+      tv.focus(tv.selection())
+      self.update_tree(None)
+      next = tv.get_children(tv.selection())
+      if next == '': #Leaf node
+        response = 'leaf'
+      else:
+        tv.selection_set(next[0])
+    elif cmd == ',': #Try and get to parent
+      next = tv.parent(tv.selection())
+      if next == '': #Parent node
+        tv.selection_set(tv.get_children()[0]) #Loop to the topmost one
+      else:
+        tv.selection_set(next)
+    tv.see(tv.selection()) #Make sure we can see this
+    return response #Right now will usually return 'ok' will return 'leaf' for '.' and leaf node
