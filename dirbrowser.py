18367b070a6cc6e5a545a1be2b50d203d3f91f0e
Kaushik Ghose <kaushik.ghose@gmail.com>
2013-07-08 19:46:18 -0400

    Code cleanup

--- dirbrowser.py
+++ dirbrowser.py
@@ -4,7 +4,6 @@ By mmgp (http://stackoverflow.com/users/1832154/mmgp)
 """
 import os, sys, Tkinter as tki, ttk
 
-#class DirBrowse(tki.Frame):
 class DirBrowse():
   """The item data consist of fullpath and type.
   The iids are set as the POSIX path and therefore can be used directly.
@@ -26,31 +25,6 @@ class DirBrowse():
   def pack(self, **options):
     self.treeview.pack(**options)
 
-  def set_auto_open(self, auto_open=False):
-    """auto_open means that when we select a single node, if that node has children, we select the first child
-    and so on, until we hit a leaf.."""
-    if not hasattr(self, 'auto_open'):
-      self.auto_open = auto_open
-    if self.auto_open:
-      self.treeview.bind('<<TreeviewSelect>>', self.auto_expand, add='+')
-    else:
-      self.treeview.unbind('<<TreeviewSelect>>', self.auto_expand)
-    print self.auto_open
-
-  def auto_expand(self, event):
-    """We call this when we change selection. If our current item is a leaf node we simply return. If it has children, we select the first child..."""
-    self.update_tree(event)
-    print 'Boom!'
-    tv = self.treeview
-    node = tv.focus()
-    children = tv.get_children(node)
-    if len(children) == 0:
-      return
-    else:
-      tv.focus(children[0])
-      tv.selection_set(children[0])
-
-
   def set_initial_focus(self):
     tv = self.treeview
     node = tv.get_children()[0]
@@ -70,18 +44,12 @@ class DirBrowse():
     if self.treeview.set(node, "type") == 'back to root': #Special top node for virtual listing
       self.set_dir_root(self.start_path)
       return
-
     if self.treeview.set(node, "type") != 'directory':
       return
-
     path = self.treeview.set(node, "fullpath")
-    # Delete the possibly 'dummy' node present.
-    self.treeview.delete(*self.treeview.get_children(node))
-
-    #parent = self.treeview.parent(node)
+    self.treeview.delete(*self.treeview.get_children(node)) # Delete the possibly 'dummy' node present.
     for p in os.listdir(path):
       p = os.path.join(path, p)
-      ptype = None
       if os.path.isdir(p):
         ptype = 'directory'
       else: #We are a regular file
@@ -97,7 +65,7 @@ class DirBrowse():
 
   def virtual_flat(self, files, title='Back to real listing'):
     # Set the contents to a flat listing of files. Useful for 'virtual' folders we create on the fly
-    map(self.treeview.delete, self.treeview.get_children()) #Delete the original
+    self.treeview.delete(*self.treeview.get_children())#Delete the original
     ins = self.treeview.insert
     #Special first node, instructs us to go back to the real listing
     ptype = 'back to root'
@@ -121,3 +89,6 @@ class DirBrowse():
     tv = self.treeview
     files = self.treeview.selection()
     return [fi for fi in files if (tv.item(fi)['values'][1]!='back to root') and (tv.item(fi)['values'][1]!='dummy')] #Only exclude the virtual listing head
+
+  def save_current_state(self):
+    """."""
