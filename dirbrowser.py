e82969d9beadc2ea73d3ddd991c5246422dd0c25
Kaushik Ghose <kaushik.ghose@gmail.com>
2013-07-09 00:04:39 -0400

    Making subclass of Frame so it can be used like a widget

--- dirbrowser.py
+++ dirbrowser.py
@@ -2,9 +2,9 @@
 http://stackoverflow.com/questions/14404982/python-gui-tree-walk
 By mmgp (http://stackoverflow.com/users/1832154/mmgp)
 """
-import os, sys, Tkinter as tki, ttk
+import os, Tkinter as tki, ttk
 
-class DirBrowse():
+class DirBrowse(tki.Frame):
   """The item data consist of fullpath and type.
   The iids are set as the POSIX path and therefore can be used directly.
   The exceptions are:
@@ -12,18 +12,17 @@ class DirBrowse():
    * Dummy leaves which have not been opened yet (required to show the expanding arrow) which have a
      ptype = 'dummy'
   """
-  def __init__(self, parent, dir_root='./', **options):
+  def __init__(self, parent, dir_root=None, **options):
+    tki.Frame.__init__(self, parent)
     style = ttk.Style()
     style.map("my.Treeview",
       foreground=[('selected', 'yellow'), ('active', 'white')],
       background=[('selected', 'black'), ('active', 'black')]
     )
-    self.treeview = ttk.Treeview(parent, columns=("fullpath", "type"),show='tree',displaycolumns=(), style='my.Treeview')
-    self.set_dir_root(dir_root)
+    self.treeview = ttk.Treeview(self, columns=("fullpath", "type"),show='tree',displaycolumns=(), style='my.Treeview')
+    self.treeview.pack(expand=True, fill='both')
+    if dir_root is not None: self.set_dir_root(dir_root)
     self.treeview.bind('<<TreeviewOpen>>', self.update_tree)
-
-  def pack(self, **options):
-    self.treeview.pack(**options)
 
   def set_initial_focus(self):
     tv = self.treeview
@@ -41,9 +40,6 @@ class DirBrowse():
     self.set_initial_focus()
 
   def fill_tree(self, node):
-    if self.treeview.set(node, "type") == 'back to root': #Special top node for virtual listing
-      self.set_dir_root(self.start_path)
-      return
     if self.treeview.set(node, "type") != 'directory':
       return
     path = self.treeview.set(node, "fullpath")
@@ -63,13 +59,13 @@ class DirBrowse():
       if ptype == 'directory':
         self.treeview.insert(oid, 0, text='dummy', values=['dummy', 'dummy'])
 
-  def virtual_flat(self, files, title='Back to real listing'):
+  def virtual_flat(self, files, title='Virtual listing'):
     # Set the contents to a flat listing of files. Useful for 'virtual' folders we create on the fly
     self.treeview.delete(*self.treeview.get_children())#Delete the original
     ins = self.treeview.insert
     #Special first node, instructs us to go back to the real listing
-    ptype = 'back to root'
-    ins('','end', text=title, values=['real listing', ptype])
+    ptype = 'title'
+    ins('','end', text=title, values=['title', ptype])
     ptype = 'file'
     for file in files:
       fname = os.path.split(file)[1]
@@ -89,6 +85,3 @@ class DirBrowse():
     tv = self.treeview
     files = self.treeview.selection()
     return [fi for fi in files if (tv.item(fi)['values'][1]!='back to root') and (tv.item(fi)['values'][1]!='dummy')] #Only exclude the virtual listing head
-
-  def save_current_state(self):
-    """."""
